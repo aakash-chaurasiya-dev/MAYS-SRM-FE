46f8238b87c1e8c0bbd370024b10eae74611f283
rahullll0117 <y.rahulkumar012@gmail.com>
2026-06-08 21:16:46 +0530

    app.jsx parts route change

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ function App() {
           <Route path="/diagnosis" element={<DiagnosisPage />} />
           <Route path="/kanban" element={<KanbanPage />} />
           <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/inventory/parts" element={<OrderPartsPage />} />
 
             {/* Ticket Routes */}
             <Route path="/tickets/new" element={<NewTicketPage />} />
